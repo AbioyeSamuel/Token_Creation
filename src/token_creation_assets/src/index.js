42113574047a45baa77d9a97b223bef8a9974dbb
samuel <olaoluwaabioye99@gmail.com>
2022-10-04 15:16:05 +0100

    Initial commit.

---
+++ src/token_creation_assets/src/index.js
@@ -0,0 +1,19 @@
+import { token_creation } from "../../declarations/token_creation";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await token_creation.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
